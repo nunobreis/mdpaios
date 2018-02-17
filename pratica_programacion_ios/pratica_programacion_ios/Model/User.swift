4a6acb1302ba9828635fbbae5c1e69a0b12de247
nunobreis <nuno@catalpa.io>
2018-02-17 14:30:54 +0100

    adds an MVC structure to my project

---
+++ pratica_programacion_ios/pratica_programacion_ios/Model/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  pratica_programacion_ios
+//
+//  Created by Nuno Reis on 17/02/2018.
+//  Copyright © 2018 Nuno Reis. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    
+    //TODO: Users need a userBody and a sender variable
+    
+    
+    
+}
